ce8a96d313e3a3c641408aedeccd6ec7bc2469e4
Mosesved <safarmagogwa@gmail.com>
2026-01-30 14:22:10 +0300

    Update session_filter.py

--- Python/session_filter.py
+++ Python/session_filter.py
@@ -1,1 +1,12 @@
+# session_filter.py
+from datetime import datetime
+import config
 
+
+def is_valid_session():
+    hour = datetime.now().hour
+
+    in_london = config.LONDON_START <= hour <= config.LONDON_END
+    in_ny = config.NY_START <= hour <= config.NY_END
+
+    return in_london or in_ny
