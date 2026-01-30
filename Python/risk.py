8664875d5813baf29037e6b18aeb79cb69ccb64f
Mosesved <safarmagogwa@gmail.com>
2026-01-30 14:24:08 +0300

    Update risk.py

--- Python/risk.py
+++ Python/risk.py
@@ -1,1 +1,16 @@
+# risk.py
+import config
 
+
+def calculate_sl_tp(entry, atr_m15, direction):
+    sl_distance = atr_m15 * config.ATR_SL_MULTIPLIER
+    tp_distance = sl_distance * config.RR_MIN
+
+    if direction == "BUY":
+        sl = entry - sl_distance
+        tp = entry + tp_distance
+    else:
+        sl = entry + sl_distance
+        tp = entry - tp_distance
+
+    return sl, tp
