c64e4beaec28545d86c6de58ef00e7d39da3c94b
Mosesved <safarmagogwa@gmail.com>
2026-01-30 14:21:07 +0300

    Update discount_filter.py

--- Python/discount_filter.py
+++ Python/discount_filter.py
@@ -1,1 +1,12 @@
+# discount_filter.py
 
+def is_in_discount_or_premium(direction, swing_high, swing_low, price):
+    midpoint = (swing_high + swing_low) / 2
+
+    if direction == "BUY":
+        return price < midpoint
+
+    if direction == "SELL":
+        return price > midpoint
+
+    return False
